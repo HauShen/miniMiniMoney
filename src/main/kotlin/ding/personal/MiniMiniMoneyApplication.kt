89b9265bf0371987bfca6b135fa8866f5eb09798
jih99 <yingjih99@gmail.com>
2023-10-03 23:03:32 +0800

    first commit

---
+++ src/main/kotlin/ding/personal/MiniMiniMoneyApplication.kt
@@ -0,0 +1,11 @@
+package ding.personal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MiniMiniMoneyApplication
+
+fun main(args: Array<String>) {
+	runApplication<MiniMiniMoneyApplication>(*args)
+}
